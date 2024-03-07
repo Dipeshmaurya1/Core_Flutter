bff2146aa72c0e3cef97b661d5b3b893ee362169
Dipesh Maurya <149373441+Dipeshmaurya1@users.noreply.github.com>
2024-03-07 13:51:29 +0530

    Update Q4.dart

--- Lecture 1/Q4.dart
+++ Lecture 1/Q4.dart
@@ -5,6 +5,6 @@ void main() {
   int number = int.parse(stdin.readLineSync()!);
 
   for (int i = 1; i <= 10; i++) {
-    print("$number * $i = ${number * i}");
+    print("$number X $i = ${number * i}");
   }
 }
